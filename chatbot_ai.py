fc55d60bfaa9b8610006d64ba7bf0fa83869012f
ramadhanilipi <90433369+ramadhanilipi@users.noreply.github.com>
2023-05-11 09:14:30 +0700

    Update chatbot_ai.py

--- chatbot_ai.py
+++ chatbot_ai.py
@@ -1,3 +1,5 @@
+#source code: https://medium.com/@avra42/build-your-own-chatbot-with-openai-gpt-3-and-streamlit-6f1330876846
+
 import openai
 import streamlit as st
 from streamlit_chat import message
